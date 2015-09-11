5a810bc089de4cb0bb509612d46774202c305af4
Xeaphii <muhammadzeeshan020@gmail.com>
2015-09-11 08:51:43 +0500

    lazy loading

--- app/src/main/java/com/example/administrator/newsexplorer/adapter/MemberListAdapter.java
+++ app/src/main/java/com/example/administrator/newsexplorer/adapter/MemberListAdapter.java
@@ -1,18 +1,32 @@
 package com.example.administrator.newsexplorer.adapter;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.administrator.newsexplorer.R;
+import com.example.administrator.newsexplorer.StorageSharedPref;
 import com.example.administrator.newsexplorer.model.MemberModel;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -23,6 +37,8 @@ public class MemberListAdapter  extends ArrayAdapter<MemberModel> {
     public final Context context;
     private final List<MemberModel> values;
     ImageLoader imageLoader;
+    StorageSharedPref sharedStorage;
+    boolean IsMoreData = true;
 
     public MemberListAdapter(Context context, List<MemberModel>  values) {
         super(context, R.layout.member_list_item, values);
@@ -30,7 +46,7 @@ public class MemberListAdapter  extends ArrayAdapter<MemberModel> {
         this.values = values;
         imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(context));
-
+        sharedStorage = new StorageSharedPref(context);
     }
 
     @Override
@@ -57,8 +73,91 @@ public class MemberListAdapter  extends ArrayAdapter<MemberModel> {
 //        } else {
 //            imageView.setImageResource(R.drawable.android_logo);
 //        }
+        if(IsMoreData&&position == values.size()-1){
+            if (isNetworkAvailable()) {
+                new uploadToServer().execute(new String[]{values.get(position).id});} else {
+                Toast.makeText(context, "No network present", Toast.LENGTH_LONG).show();
 
+            }
+        }
         return rowView;
     }
+    class uploadToServer extends AsyncTask<String, Void, String> {
 
+        private ProgressDialog pd = new ProgressDialog(context);
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd.setMessage("Getting more members!");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            try {
+                //------------------>>
+                HttpGet httppost = new HttpGet(("http://ghanchidarpan.org/news/get_members.php?user_id=" +
+                        encodeHTML(sharedStorage.GetPrefs("user_id","")+"" +
+                                "&id_from=" +
+                                params[0]) ).replaceAll(" ", "%20") );
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httppost);
+
+                // StatusLine stat = response.getStatusLine();
+                int status = response.getStatusLine().getStatusCode();
+
+                if (status == 200) {
+                    HttpEntity entity = response.getEntity();
+                    String resp = EntityUtils.toString(entity).trim();
+                    if(resp.equals("")){
+                        IsMoreData = false;
+                    }else{
+                        String[] data = resp.split(";",-1);
+                        for(int i = 0 ; i < data.length;i++){
+                            values.add(new MemberModel(data[i].split(":",-1)[1],data[i].split(":",-1)[0],data[i].split(":",-1)[2].trim()));
+                        }
+                    }
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+            return "";
+
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            pd.hide();
+            pd.dismiss();
+            notifyDataSetChanged();
+        }
+    }
+    public static String encodeHTML(String s)
+    {
+        StringBuffer out = new StringBuffer();
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c > 127 || c=='"' || c=='<' || c=='>')
+            {
+                out.append("&#"+(int)c+";");
+            }
+            else
+            {
+                out.append(c);
+            }
+        }
+        return out.toString();
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 }
