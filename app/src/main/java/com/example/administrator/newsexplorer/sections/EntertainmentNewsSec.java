4ae0f40508eda550b0b85e2ed6e7607610f078f9
Xeaphii <muhammadzeeshan020@gmail.com>
2015-09-12 11:25:10 -0700

    Redesign

--- app/src/main/java/com/example/administrator/newsexplorer/sections/EntertainmentNewsSec.java
+++ app/src/main/java/com/example/administrator/newsexplorer/sections/EntertainmentNewsSec.java
@@ -1,8 +1,15 @@
 package com.example.administrator.newsexplorer.sections;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.example.administrator.newsexplorer.R;
 
@@ -10,6 +17,7 @@ import com.example.administrator.newsexplorer.R;
  * Created by Xeaphii on 9/6/2015.
  */
 public class EntertainmentNewsSec extends Activity {
+    WebView webDesigner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,13 @@ public class EntertainmentNewsSec extends Activity {
         setContentView(R.layout.entertainment_news_sec);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
+        webDesigner = (WebView) findViewById(R.id.web_designer);
+        if(isNetworkAvailable()){
+            webDesigner.loadUrl("http://ghanchidarpan.org/wp_site/wordpress/category/entertainment/");
+            webDesigner.setWebViewClient(new WebViewClient());
+        }else {
+            Toast.makeText(getApplicationContext(), "No internet connection present", Toast.LENGTH_LONG).show();
+        }
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -28,5 +43,28 @@ public class EntertainmentNewsSec extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch(keyCode)
+            {
+                case KeyEvent.KEYCODE_BACK:
+                    if(webDesigner.canGoBack()){
+                        webDesigner.goBack();
+                    }else{
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
